e856d8b61ea17672d360a3e0ea8c908da3c27934
blowedminds <ruzgardeniz.08@hotmail.com>
2018-05-27 00:04:00 +0300

    Project files added

---
+++ src/project/MerkleTree.java
@@ -0,0 +1,46 @@
+package project;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class MerkleTree {
+
+    private String chunksPath;
+
+    private Node root;
+
+    public MerkleTree(String path) {
+
+        this.chunksPath = path;
+
+        this.createMT();
+    }
+
+    public Node getRoot() {
+        return this.root;
+    }
+
+    public boolean checkAuthenticity(String trustedSource) {
+        return false;
+    }
+
+    public ArrayList<Stack<String>> findCorruptChunks(String metaFile) {
+        return null;
+    }
+
+    private void createMT() {
+
+    }
+
+    private ArrayList<String> readAndParsePaths() {
+
+        return null;
+    }
+
+    private BufferedReader readFile(String path) {
+        return null;
+    }
+}
---
+++ src/project/Node.java
@@ -0,0 +1,37 @@
+package project;
+
+public class Node {
+
+    private String data;
+
+    private Node left, right;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return this.right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+}
